3c75aa183dcb84d2890c7427d01c1a2f17667c93
Ian Yong <ianyongyc@gmail.com>
2018-11-26 00:49:04 +0800

    Updated dependencies (including migration to AndroidX), updated .gitignore, and updated compileSdkVersion and targetSdkVersion to 28

--- app/src/main/java/io/github/ianyong/spfdivisionalboundaries/MapsActivity.java
+++ app/src/main/java/io/github/ianyong/spfdivisionalboundaries/MapsActivity.java
@@ -1,6 +1,6 @@
 package io.github.ianyong.spfdivisionalboundaries;
 
-import android.support.v4.app.FragmentActivity;
+import androidx.fragment.app.FragmentActivity;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
