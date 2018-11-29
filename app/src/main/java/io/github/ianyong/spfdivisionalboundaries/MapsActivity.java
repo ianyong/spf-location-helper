9f905376235ce7919b635f0c171f10dd35581a01
Ian Yong <ianyongyc@gmail.com>
2018-11-29 20:42:33 +0800

    Moved compass down to account for the floating search bar

--- app/src/main/java/io/github/ianyong/spfdivisionalboundaries/MapsActivity.java
+++ app/src/main/java/io/github/ianyong/spfdivisionalboundaries/MapsActivity.java
@@ -2,14 +2,11 @@ package io.github.ianyong.spfdivisionalboundaries;
 
 import androidx.fragment.app.FragmentActivity;
 
-import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.arlib.floatingsearchview.FloatingSearchView;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
-import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -23,16 +20,16 @@ import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    private GoogleMap googleMap;
     private FloatingSearchView searchView;
-    private List<LocationSuggestion> list;
+    private List<LocationSuggestion> suggestionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        list = new ArrayList<>();
+        suggestionList = new ArrayList<>();
         searchView = findViewById(R.id.floating_search_view);
         searchView.setOnQueryChangeListener(new FloatingSearchView.OnQueryChangeListener() {
             @Override
@@ -40,19 +37,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 if (!oldQuery.equals("") && newQuery.equals("")) {
                     searchView.clearSuggestions();
                 } else {
-                    list.clear();
-                    list.add(new LocationSuggestion("a"));
-                    list.add(new LocationSuggestion("b"));
-                    list.add(new LocationSuggestion("c"));
-                    list.add(new LocationSuggestion("d"));
-                    searchView.swapSuggestions(list);
+                    suggestionList.clear();
+                    suggestionList.add(new LocationSuggestion("a"));
+                    suggestionList.add(new LocationSuggestion("b"));
+                    suggestionList.add(new LocationSuggestion("c"));
+                    suggestionList.add(new LocationSuggestion("d"));
+                    searchView.swapSuggestions(suggestionList);
                 }
             }
         });
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        // Obtain the SupportMapFragment
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
+        // Move compass location
+        View compassButton = mapFragment.getView().findViewWithTag("GoogleMapCompass");
+        RelativeLayout.LayoutParams rlp = (RelativeLayout.LayoutParams) compassButton.getLayoutParams();
+        rlp.topMargin = (int) (getResources().getDimension(R.dimen.sliding_search_view_header_height)/getResources().getDisplayMetrics().density) + rlp.leftMargin;
+
+        // Get notified when the map is ready to be used.
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
@@ -70,17 +74,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        this.googleMap = googleMap;
 
         LatLng singapore = new LatLng(1.352083, 103.819836); // Center of Singapore
         LatLngBounds viewportBounds = new LatLngBounds(new LatLng(1.1496, 103.594), new LatLng(1.4784001, 104.0945001));
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(singapore, 10));
-        mMap.setLatLngBoundsForCameraTarget(viewportBounds);
-        mMap.setMinZoomPreference(10.0f);
+        this.googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(singapore, 10));
+        this.googleMap.setLatLngBoundsForCameraTarget(viewportBounds);
+        this.googleMap.setMinZoomPreference(10.0f);
 
         try {
-            KmlLayer NPCs = new KmlLayer(mMap, R.raw.singapore_police_force_npc_boundary_kml, getApplicationContext());
+            KmlLayer NPCs = new KmlLayer(this.googleMap, R.raw.singapore_police_force_npc_boundary_kml, getApplicationContext());
             NPCs.addLayerToMap();
         } catch(Exception e) {
             e.printStackTrace();
