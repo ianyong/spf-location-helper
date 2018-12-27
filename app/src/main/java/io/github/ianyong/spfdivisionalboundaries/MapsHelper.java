1d736263b5c3d588bf0d8e33e889996a52c44bbe
Ian Yong <ianyongyc@gmail.com>
2018-12-27 18:56:42 +0800

    Added reverse geocode on long click

--- app/src/main/java/io/github/ianyong/spfdivisionalboundaries/MapsHelper.java
+++ app/src/main/java/io/github/ianyong/spfdivisionalboundaries/MapsHelper.java
@@ -1,6 +1,12 @@
 package io.github.ianyong.spfdivisionalboundaries;
 
 import android.content.Context;
+import android.content.Intent;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.ResultReceiver;
 
 import com.google.android.gms.location.places.AutocompletePrediction;
 import com.google.android.gms.location.places.Place;
@@ -14,6 +20,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
+import java.util.Locale;
+
 import androidx.annotation.NonNull;
 
 public class MapsHelper {
@@ -22,11 +30,16 @@ public class MapsHelper {
     private GoogleMap googleMap;
     private int screenWidth, screenHeight;
     private Marker marker;
+    private Geocoder geocoder;
+    private AddressResultReceiver resultReceiver;
+    private Location selectedLocation;
 
     public MapsHelper(Context context) {
         this.context = context;
         screenWidth = context.getResources().getDisplayMetrics().widthPixels;
         screenHeight = context.getResources().getDisplayMetrics().heightPixels;
+        geocoder = new Geocoder(context, Locale.getDefault());
+        resultReceiver = new AddressResultReceiver(new Handler());
     }
 
     public void processLocation(AutocompletePrediction autocompletePrediction) {
@@ -66,8 +79,44 @@ public class MapsHelper {
             @Override
             public void onMapLongClick(LatLng latLng) {
                 addMarker(latLng, "");
+                selectedLocation = new Location("MapLongClick");
+                selectedLocation.setLatitude(latLng.latitude);
+                selectedLocation.setLongitude(latLng.longitude);
+                // Start reverse geocode.
+                startIntentService();
             }
         });
+    }
+
+    private void startIntentService() {
+        Intent intent = new Intent(context, FetchAddressIntentService.class);
+        intent.putExtra(Constants.RECEIVER, resultReceiver);
+        intent.putExtra(Constants.LOCATION_DATA_EXTRA, selectedLocation);
+        context.startService(intent);
+    }
+
+    class AddressResultReceiver extends ResultReceiver {
+
+        public AddressResultReceiver(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+            if (resultData == null) {
+                return;
+            }
+            String addressOutput = resultData.getString(Constants.RESULT_DATA_KEY);
+            if (addressOutput == null) {
+                addressOutput = "";
+            }
+            if (resultCode == Constants.SUCCESS_RESULT) {
+                // Update marker with reverse geocode result.
+                marker.setTitle(addressOutput);
+                marker.showInfoWindow();
+            }
+        }
+
     }
 
 }
